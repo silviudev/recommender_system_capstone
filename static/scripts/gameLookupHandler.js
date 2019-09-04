c45c6179c5464fbce62daf1a76ebab8dc419c8b2
silviudev <silviudev@yahoo.com>
2019-09-04 16:25:05 -0700

    Layout of recommender page complete

--- static/scripts/gameLookupHandler.js
+++ static/scripts/gameLookupHandler.js
@@ -5,6 +5,16 @@ $(function() {
       $("#gameName").html(data["mainData"]["0"]["1"]);
       $("#gameImage").attr("src", data["mainData"]["0"]["13"]);
       $("#gameDescription").html(data["mainData"]["0"]["12"]);
+      $("#gameID").html("ID: " + data["mainData"]["0"]["0"])
+      $("#gameTags").html("Tags: " + data["mainData"]["0"]["7"].split(";").join(", "))
+      $("#gameGenre").html("Genres: " + data["mainData"]["0"]["6"])
+      $("#gamePrice").html("Price: " + data["mainData"]["0"]["11"])
+      $("#gameDate").html("Release Date: " + data["mainData"]["0"]["2"])
+      $("#gameDeveloper").html("Developed by " + data["mainData"]["0"]["3"])
+      $("#gamePublisher").html("Published By " + data["mainData"]["0"]["4"])
+      $("#gamePositive").html("Postivie Ratings: " + data["mainData"]["0"]["8"])
+      $("#gameNegative").html("Negative Ratings: " + data["mainData"]["0"]["9"])
+      $("#gamePlatforms").html("Platform: " + data["mainData"]["0"]["5"].split(";").join(", "))
 
       for(let i = 0; i < data["recData"].length; i++){
         let queryString = "#rec" + (i+1)
